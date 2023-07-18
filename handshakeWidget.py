714a092bdb0730917ff6c249883c5ac2c2959ee5
Davazano <davideoguche@gmail.com>
2023-07-18 09:23:04 +0100

    Added regular and modified handshake buttons in horizontal layout in handshakeWidget file

--- handshakeWidget.py
+++ handshakeWidget.py
@@ -1,8 +1,21 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QPushButton, QWidget, QHBoxLayout, QVBoxLayout
 
 # main window class for 4 way handshake widget
-class HandshakeWidget(QMainWindow):
+class HandshakeWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("4 Way Handshake Simulator")
+
+        btnRegularHandshake = QPushButton("Regular Handshake Protocol")
+        #btnRegularHandshake.clicked.connect(self.simulateRegularHandshake)
+
+        btnModifiedHandshake = QPushButton("Modified Handshake Protocol")
+        #btnModifiedHandshake.clicked.connect(self.simulateModifiedHandshake)
+
+        #Horizontal arrangement
+        widgetLayout = QHBoxLayout()
+        widgetLayout.addWidget(btnRegularHandshake)
+        widgetLayout.addWidget(btnModifiedHandshake)
+
+        self.setLayout(widgetLayout)
         
